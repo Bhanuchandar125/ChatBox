abf53ec729eb4caf2cd00c953f5715a74655ff36
Bhanu <bcr.chilukoti@gmail.com>
2023-08-23 19:52:59 +0530

    day4

--- src/Components/ChatContainer.tsx
+++ src/Components/ChatContainer.tsx
@@ -1,10 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ChatContainer.css";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 import { ExpandMore } from "@mui/icons-material";
-import { BiVideo } from "react-icons/bi";
+import { BiUpload, BiVideo } from "react-icons/bi";
+
+// import "emoji-mart/css/emoji-mart.css";
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+import EmojiEmotionsOutlinedIcon from "@mui/icons-material/EmojiEmotionsOutlined";
+import { AiOutlineSend } from "react-icons/ai";
 
 const ChatContainer = () => {
+  const [message, setMessage] = useState<any>("");
+  const [sentmessages, setSentMessages] = useState<any>([]);
+  const [isEmojiOpen, setEmojiOpen] = useState<boolean>(false);
+
+  const handleEmojiSelect = (emoji: any) => {
+    setMessage([...message, emoji.native]);
+  };
+
+  const handleSend = () => {
+    if (message.trim() !== "") {
+      setSentMessages([...sentmessages, message]);
+      setMessage("");
+    }
+    
+  };
+  console.log("sent", sentmessages);
+  const handlechangeMessage = (e: any) => {
+    const value = e.target.value;
+    setMessage(value);
+  };
+  console.log(message);
   return (
     <div className="chatContainer">
       <div className="chatHeader">
@@ -23,9 +50,41 @@ const ChatContainer = () => {
       </div>
       <hr />
       <div className="chatBody">
-        <h1>Hello</h1>
+        <ul className="listcontainer">
+          {sentmessages.map((each: any, index: number) => {
+            return <li className='messagecontainer' key={index}>{each}</li>
+          })}
+        </ul>
       </div>
-      <div className="chattyping"></div>
+      <div className="messageinput">
+        <input
+          type="text"
+          className="textinput"
+          onChange={handlechangeMessage}
+          placeholder="Type Message..."
+          value={message}
+        />
+        <label htmlFor="fileInput">
+          <BiUpload className="customFileInput" />
+        </label>
+        <input
+          type="file"
+          id="fileInput"
+          // onChange={handleFileUpload}
+          style={{ display: "none" }}
+        />
+        <label className="emojibg" onClick={() => setEmojiOpen(!isEmojiOpen)}>
+          <EmojiEmotionsOutlinedIcon />
+        </label>
+        {isEmojiOpen && (
+          <div className="emojiPickerContainer">
+            <Picker data={data} onEmojiSelect={handleEmojiSelect} />
+          </div>
+        )}
+        <label onClick={handleSend}>
+          <AiOutlineSend className="sendIcon" />
+        </label>
+      </div>
     </div>
   );
 };
